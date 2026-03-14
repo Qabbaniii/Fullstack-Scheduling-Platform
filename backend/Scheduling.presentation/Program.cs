e822ec6b3dfc9b91049eb3d61152671c001554e2
Qabbaniii <qabbaniofficial@gmail.com>
2026-03-14 05:10:20 +0200

    Deploy changes

--- backend/Scheduling.presentation/Program.cs
+++ backend/Scheduling.presentation/Program.cs
@@ -96,11 +96,10 @@ builder.Configuration
 
             // Configure the HTTP request pipeline.
 
-            if (app.Environment.IsDevelopment())
-            {
+    
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+
 
             app.UseHttpsRedirection();
             app.UseCors("AllowFrontend");
